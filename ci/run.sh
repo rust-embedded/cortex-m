66a17696addb7bca7c94f2ed605e53fc28e76263
Jorge Aparicio <japaricious@gmail.com>
2016-10-06 23:33:09 -0500

    don't install Xargo from within the docker container

--- ci/run.sh
+++ ci/run.sh
@@ -4,8 +4,6 @@ set -ex
 
 case $1 in
     thumbv*)
-        curl -sf "https://raw.githubusercontent.com/japaric/rust-everywhere/master/install.sh" | \
-            bash -s -- --at /usr/bin --from japaric/xargo --tag v0.1.9
         xargo build --target $1
     ;;
     *)

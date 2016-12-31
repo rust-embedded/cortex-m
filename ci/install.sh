3dbfae5010bc5dfdf1486bceeab2f12a629247c8
Jorge Aparicio <japaricious@gmail.com>
2016-12-31 18:14:01 -0500

    call `source` from "outside"

--- ci/install.sh
+++ ci/install.sh
@@ -4,10 +4,13 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    source ~/.cargo/env
-
     curl -LSfs http://japaric.github.io/trust/install.sh | \
-        sh -s -- --force --git japaric/cross --tag v0.1.2 --target $TARGET
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag v0.1.2 \
+           --target $TARGET \
+           --to ~/.cargo/bin
 }
 
 # NOTE(TRAVIS_BRANCH) Travis is configured to only build *pushes* (not PRs)

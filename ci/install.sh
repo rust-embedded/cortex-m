fc9e6762187941b5a0457acef6899dfacc552d53
Jorge Aparicio <jorge@japaric.io>
2017-05-30 12:02:49 -0500

    update the CI stuff

--- ci/install.sh
+++ ci/install.sh
@@ -1,21 +1,7 @@
 set -ex
 
 main() {
-    curl https://sh.rustup.rs -sSf | \
-        sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
-
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0-9.]+$' \
-                       | sort --version-sort \
-                       | tail -n1)
-    curl -LSfs http://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag \
-           --target x86_64-unknown-linux-musl \
-           --to ~/.cargo/bin
+    return
 }
 
 # NOTE(TRAVIS_BRANCH) Travis is configured to only build *pushes* (not PRs)

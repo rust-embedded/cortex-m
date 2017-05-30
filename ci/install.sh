db27f0b60c851db013aa9a730b9b4f71ce25f004
Jorge Aparicio <jorge@japaric.io>
2017-05-30 13:10:35 -0500

    fix rust-src check

--- ci/install.sh
+++ ci/install.sh
@@ -3,8 +3,10 @@ set -ex
 main() {
     case $TARGET in
         thumbv*-none-eabi*)
-            cargo install --list | grep xargo || cargo install xargo
-            rustup component list | grep rust-src || rustup component add rust-src
+            cargo install --list | grep xargo || \
+                cargo install xargo
+            rustup component list | grep 'rust-src.*installed' || \
+                rustup component add rust-src
             ;;
     esac
 }

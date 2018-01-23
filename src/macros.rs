d30209fa372831d819607e6a324b7512f8d1cfaa
Jorge Aparicio <jorge@japaric.io>
2018-01-23 20:27:06 +0100

    initialize singletons at runtime

--- src/macros.rs
+++ src/macros.rs
@@ -54,13 +54,14 @@ macro_rules! singleton {
     (: $ty:ty = $expr:expr) => {
         $crate::interrupt::free(|_| unsafe {
             static mut USED: bool = false;
-            static mut VAR: $ty = $expr;
+            static mut VAR: $crate::UntaggedOption<$ty> = $crate::UntaggedOption { none: () };
 
             if USED {
                 None
             } else {
                 USED = true;
-                let var: &'static mut _ = &mut VAR;
+                VAR.some = $expr;
+                let var: &'static mut _ = &mut VAR.some;
                 Some(var)
             }
         })

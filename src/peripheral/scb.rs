733e6d6f1bbd99346d74f8d23310065335f4f04c
Rajiv Ranganath <rajiv.ranganath@gmail.com>
2018-07-13 19:52:38 +0530

    Rename `shcrs` to `shcsr` in `scb::RegisterBlock`
    Commit `c290aa4e` introduced `shcrs` field to `scb::RegisterBlock`. 
    
    In CMSIS, this field is `shcsr`.
    
    https://github.com/ARM-software/CMSIS_5/blob/5.3.0/CMSIS/Core/Include/core_cm4.h#L449
    
    This patch changes `shcrs` to `shcsr`.
    
    Signed-off-by: Rajiv Ranganath <rajiv.ranganath@gmail.com>

--- src/peripheral/scb.rs
+++ src/peripheral/scb.rs
@@ -51,7 +51,7 @@ pub struct RegisterBlock {
     pub shpr: [RW<u32>; 2],
 
     /// System Handler Control and State
-    pub shcrs: RW<u32>,
+    pub shcsr: RW<u32>,
 
     /// Configurable Fault Status (not present on Cortex-M0 variants)
     #[cfg(not(armv6m))]

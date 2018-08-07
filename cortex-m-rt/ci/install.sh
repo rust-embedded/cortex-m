b13192d0aaf011eb9bcb8498577d537f0d1c0b64
Jorge Aparicio <jorge@japaric.io>
2018-08-07 16:12:29 -0500

    install a newer gcc

--- cortex-m-rt/ci/install.sh
+++ cortex-m-rt/ci/install.sh
@@ -3,6 +3,12 @@ set -euxo pipefail
 main() {
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
         rustup target add $TARGET
+
+        if [ ${CC:-gcc} = gcc ]; then
+            sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa -y
+            sudo apt-get update -q
+            sudo apt-get install gcc-arm-embedded -y
+        fi
     fi
 }
 

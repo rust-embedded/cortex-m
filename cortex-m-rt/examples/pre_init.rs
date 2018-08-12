743b8fc282f16ab54ff9ff2540f8db337f3562ad
Gabriel Smith <ga29smith@gmail.com>
2018-08-12 16:31:17 -0400

    Remove the setting of exceptions from the pre_init example as they are now defaulted
    Signed-off-by: Gabriel Smith <ga29smith@gmail.com>

--- cortex-m-rt/examples/pre_init.rs
+++ cortex-m-rt/examples/pre_init.rs
@@ -4,11 +4,9 @@
 #![no_main]
 #![no_std]
 
-#[macro_use(entry, exception, pre_init)]
+#[macro_use(entry, pre_init)]
 extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
-
-use rt::ExceptionFrame;
 
 pre_init!(disable_watchdog);
 
@@ -22,15 +20,3 @@ entry!(main);
 fn main() -> ! {
     loop {}
 }
-
-// the hard fault handler
-exception!(HardFault, hard_fault);
-
-fn hard_fault(_ef: &ExceptionFrame) -> ! {
-    loop {}
-}
-
-// the default exception handler
-exception!(*, default_handler);
-
-fn default_handler(_irqn: i16) {}

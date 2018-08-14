0b9da3dcdbf8186521218a7e319be1029d592935
Brandon Matthews <bmatthews@zipcar.com>
2018-08-14 10:26:32 -0700

    Add support for debugger check

--- src/peripheral/dcb.rs
+++ src/peripheral/dcb.rs
@@ -20,7 +20,7 @@ pub struct RegisterBlock {
 }
 
 impl DCB {
-    /// Enables TRACE. This is for example required by the 
+    /// Enables TRACE. This is for example required by the
     /// `peripheral::DWT` cycle counter to work properly.
     /// As by STM documentation, this flag is not reset on
     /// soft-reset, only on power reset.
@@ -28,10 +28,14 @@ impl DCB {
         // set bit 24 / TRCENA
         unsafe { self.demcr.modify(|w| w | DCB_DEMCR_TRCENA); }
     }
-    
+
     /// Disables TRACE. See `DCB::enable_trace()` for more details
     pub fn disable_trace(&mut self) {
         // unset bit 24 / TRCENA
         unsafe { self.demcr.modify(|w| w & !DCB_DEMCR_TRCENA); }
+
+    /// Is there a debugger attached?
+    pub fn is_debugger_attached(&self) -> bool {
+        self.dhcsr.read() & 0x1 == 1
     }
 }

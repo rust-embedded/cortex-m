13512674413ccf84f1abc12c0fa9b57d6045661e
Jorge Aparicio <jorge@japaric.io>
2018-08-25 15:31:06 +0200

    refactor assemble.sh

--- cortex-m-rt/assemble.sh
+++ cortex-m-rt/assemble.sh
@@ -4,14 +4,16 @@ set -euxo pipefail
 
 # cflags taken from cc 1.0.22
 
-arm-none-eabi-as -march=armv6s-m asm.s -o bin/cortex-m-rt.o
-ar crs bin/thumbv6m-none-eabi.a bin/cortex-m-rt.o
+crate=cortex-m-rt
 
-arm-none-eabi-as -march=armv7-m asm.s -o bin/cortex-m-rt.o
-ar crs bin/thumbv7m-none-eabi.a bin/cortex-m-rt.o
+arm-none-eabi-as -march=armv6s-m asm.s -o bin/$crate.o
+ar crs bin/thumbv6m-none-eabi.a bin/$crate.o
 
-arm-none-eabi-as -march=armv7e-m asm.s -o bin/cortex-m-rt.o
-ar crs bin/thumbv7em-none-eabi.a bin/cortex-m-rt.o
-ar crs bin/thumbv7em-none-eabihf.a bin/cortex-m-rt.o
+arm-none-eabi-as -march=armv7-m asm.s -o bin/$crate.o
+ar crs bin/thumbv7m-none-eabi.a bin/$crate.o
 
-rm bin/cortex-m-rt.o
+arm-none-eabi-as -march=armv7e-m asm.s -o bin/$crate.o
+ar crs bin/thumbv7em-none-eabi.a bin/$crate.o
+ar crs bin/thumbv7em-none-eabihf.a bin/$crate.o
+
+rm bin/$crate.o

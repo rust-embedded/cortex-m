afb4dca1abb0f81b26abdef765a7d5b7814c3c8f
kellerkindt <michael@kellerkindt.com>
2018-09-02 11:09:11 +0200

    Add DCB::enable_trace() and DCB::disable_trace()

--- src/peripheral/dcb.rs
+++ src/peripheral/dcb.rs
@@ -2,6 +2,8 @@
 
 use volatile_register::{RW, WO};
 
+use peripheral::DCB;
+
 /// Register block
 #[repr(C)]
 pub struct RegisterBlock {
@@ -13,4 +15,19 @@ pub struct RegisterBlock {
     pub dcrdr: RW<u32>,
     /// Debug Exception and Monitor Control
     pub demcr: RW<u32>,
+}
+
+impl DCB {
+    /// Enables TRACE. This is for example required by the 
+    /// `peripheral::DWT` cycle counter to work properly.
+    /// As by STM documentation, this flag is not reset on
+    /// soft-reset, only on power reset.
+    pub fn enable_trace(&mut self) {
+        unsafe { self.demcr.modify(|w| w | 0x01000000); }
+    }
+    
+    /// Disables TRACE. See `DCB::enable_trace()` for more details
+    pub fn disable_trace(&mut self) {
+        unsafe { self.demcr.modify(|w| w & !0x01000000); }
+    }
 }

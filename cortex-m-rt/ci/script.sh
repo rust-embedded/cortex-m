1831c766e74b45e05745fb0c8e68e069cfe817d6
Hideki Sekine <sekineh@me.com>
2018-09-25 07:14:19 +0900

    [ci] run qemu for both GNU LD and rustc's LLD.

--- cortex-m-rt/ci/script.sh
+++ cortex-m-rt/ci/script.sh
@@ -70,6 +70,12 @@ main() {
 
     case $TARGET in
         thumbv6m-none-eabi|thumbv7m-none-eabi)
+            # linking with GNU LD
+            cargo run --target $TARGET --example qemu \
+                  -C linker=arm-none-eabi-ld | grep "x = 42"
+            cargo run --target $TARGET --example qemu --release \
+                  -C linker=arm-none-eabi-ld | grep "x = 42"
+            # linking with rustc's LLD
             cargo run --target $TARGET --example qemu | grep "x = 42"
             cargo run --target $TARGET --example qemu --release | grep "x = 42"
             ;;

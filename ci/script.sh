e2bda1a5cdda37cec2ea9e1aab2e990f7701da3a
Jorge Aparicio <jorge@japaric.io>
2018-10-26 19:58:32 +0200

    deny warnings only on nightly
    lints have changed between nightly and stable

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,10 @@
 set -euxo pipefail
 
 main() {
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+        export RUSTFLAGS="-D warnings"
+    fi
+
     cargo check --target $TARGET
 
     if [ $TRAVIS_RUST_VERSION = nightly ]; then

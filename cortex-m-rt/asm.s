e883c5e962dd7fb420554787010df7f1a10c9926
Jorge Aparicio <jorge@japaric.io>
2018-10-27 14:19:28 +0200

    rename UserHardFault to HardFault
    so it matches the exception name (`#[exception] fn HardFault(..`)

--- cortex-m-rt/asm.s
+++ cortex-m-rt/asm.s
@@ -1,10 +1,10 @@
   # LLD requires that the section flags are explicitly set here
-  .section .HardFault, "ax"
-  .global HardFault
+  .section .HardFaultTrampoline, "ax"
+  .global HardFaultTrampoline
   # .type and .thumb_func are both required; otherwise its Thumb bit does not
   # get set and an invalid vector table is generated
-  .type HardFault,%function
+  .type HardFaultTrampoline,%function
   .thumb_func
-HardFault:
+HardFaultTrampoline:
   mrs r0, MSP
-  b UserHardFault
+  b HardFault

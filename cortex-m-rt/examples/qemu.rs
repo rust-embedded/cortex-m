bb25ff4507f72859b4b6f09a0b81632873053abf
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-03-16 22:46:18 +0000

    Bump cortex-m dev-dep to 0.6 (it's only a dev-dep and so doesn't affect other packages) Disable the semihosting test on thumbv8, as it's currently unsupported in cortex-m-semihosting

--- cortex-m-rt/examples/qemu.rs
+++ cortex-m-rt/examples/qemu.rs
@@ -2,18 +2,22 @@
 #![no_main]
 #![no_std]
 
-extern crate cortex_m;
 
+extern crate cortex_m;
 extern crate cortex_m_rt as rt;
+
+#[cfg(not(armv8m))]
 extern crate cortex_m_semihosting as semihosting;
+
 extern crate panic_halt;
 
-use core::fmt::Write;
 use cortex_m::asm;
 use rt::entry;
 
+#[cfg(not(armv8m))]
 #[entry]
 fn main() -> ! {
+    use core::fmt::Write;
     let x = 42;
 
     loop {
@@ -21,9 +25,16 @@ fn main() -> ! {
 
         // write something through semihosting interface
         let mut hstdout = semihosting::hio::hstdout().unwrap();
-        write!(hstdout, "x = {}\n", x);
-
+        write!(hstdout, "x = {}\n", x).unwrap();
         // exit from qemu
         semihosting::debug::exit(semihosting::debug::EXIT_SUCCESS);
+    }
+}
+
+#[cfg(armv8m)]
+#[entry]
+fn main() -> ! {
+    loop {
+        asm::nop();
     }
 }

ac17dbd7ec1e04954b9684bcb50595665c7dcf9c
Jonas Schievink <jonasschievink@gmail.com>
2019-09-10 17:44:50 +0200

    Address shellcheck warnings

--- cortex-m-rt/check-blobs.sh
+++ cortex-m-rt/check-blobs.sh
@@ -4,18 +4,18 @@
 
 set -euxo pipefail
 
-for lib in $(ls bin/*.a); do
-    filename=$(basename $lib)
-    arm-none-eabi-objdump -Cd $lib > bin/${filename%.a}.before
+for lib in bin/*.a; do
+    filename=$(basename "$lib")
+    arm-none-eabi-objdump -Cd "$lib" > "bin/${filename%.a}.before"
 done
 
 ./assemble.sh
 
-for lib in $(ls bin/*.a); do
-    filename=$(basename $lib)
-    arm-none-eabi-objdump -Cd $lib > bin/${filename%.a}.after
+for lib in bin/*.a; do
+    filename=$(basename "$lib")
+    arm-none-eabi-objdump -Cd "$lib" > "bin/${filename%.a}.after"
 done
 
-for cksum in $(ls bin/*.after); do
-    diff -u $cksum ${cksum%.after}.before
+for cksum in bin/*.after; do
+    diff -u "$cksum" "${cksum%.after}.before"
 done

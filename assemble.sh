808aa0c7ead9fbdd4d434a8093e0e9e21bd5b99d
Dániel Buga <daniel@revolutionrobotics.org>
2020-05-28 00:06:33 +0200

    Update blobs

--- assemble.sh
+++ assemble.sh
@@ -17,7 +17,7 @@ arm-none-eabi-as -g -march=armv7-m asm-v7.s -o bin/$crate-v7.o
 ar crs bin/thumbv7m-none-eabi.a bin/$crate.o bin/$crate-v7.o
 
 arm-none-eabi-as -g -march=armv7e-m asm.s -o bin/$crate.o
-arm-none-eabi-as -g -march=armv7e-m asm-fpu.s -o bin/$crate-v7-fpu.o
+arm-none-eabi-as -g -march=armv7e-m asm-fpu.s -mfpu=fpv4-sp-d16 -o bin/$crate-v7-fpu.o
 arm-none-eabi-as -g -march=armv7e-m asm-cm7-r0p1.s -o bin/$crate-cm7-r0p1.o
 arm-none-eabi-as -g -march=armv7e-m asm-v7.s -o bin/$crate-v7.o
 ar crs bin/thumbv7em-none-eabi.a bin/$crate.o bin/$crate-v7.o bin/$crate-cm7-r0p1.o
@@ -31,13 +31,14 @@ arm-none-eabi-as -g -march=armv8-m.main asm.s -o bin/$crate.o
 arm-none-eabi-as -g -march=armv8-m.main asm-v7.s -o bin/$crate-v7.o
 arm-none-eabi-as -g -march=armv8-m.main asm-v8.s -o bin/$crate-v8.o
 arm-none-eabi-as -g -march=armv8-m.main asm-v8-main.s -o bin/$crate-v8-main.o
-arm-none-eabi-as -g -march=armv8-m main asm-fpu.s -o bin/$crate-v7-fpu.o
+arm-none-eabi-as -g -march=armv7e-m asm-fpu.s -mfpu=fpv5-sp-d16 -o bin/$crate-v8-fpu.o
 ar crs bin/thumbv8m.main-none-eabi.a bin/$crate.o bin/$crate-v7.o bin/$crate-v8.o bin/$crate-v8-main.o
-ar crs bin/thumbv8m.main-none-eabihf.a bin/$crate.o bin/$crate-v7.o bin/$crate-v8.o bin/$crate-v8-main.o bin/$crate-v7-fpu.o
+ar crs bin/thumbv8m.main-none-eabihf.a bin/$crate.o bin/$crate-v7.o bin/$crate-v8.o bin/$crate-v8-main.o bin/$crate-v8-fpu.o
 
 rm bin/$crate.o
 rm bin/$crate-v7.o
 rm bin/$crate-v7-fpu.o
+rm bin/$crate-v8-fpu.o
 rm bin/$crate-cm7-r0p1.o
 rm bin/$crate-v8.o
 rm bin/$crate-v8-main.o

5e9347e53485f6c676dbd06b0ded7abdf05b264b
Jonas Schievink <jonasschievink@gmail.com>
2020-08-26 01:39:26 +0200

    Only check blobs with the nightly compiler

--- ci/install.sh
+++ ci/install.sh
@@ -5,6 +5,19 @@ main() {
         thumbv*-none-eabi*)
             rustup target add $TARGET
             ;;
+        x86_64-unknown-linux-gnu)
+            # We need *all* targets and nightly as we're checking the blobs.
+            rustup install nightly
+            rustup target add \
+                thumbv6m-none-eabi \
+                thumbv7m-none-eabi \
+                thumbv7em-none-eabi \
+                thumbv7em-none-eabihf \
+                thumbv8m.base-none-eabi \
+                thumbv8m.main-none-eabi \
+                thumbv8m.main-none-eabihf \
+                --toolchain nightly
+            ;;
     esac
 }
 

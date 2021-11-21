d45bad7f6d2abdf7f1ea27868791987e87bd2876
Viktor Sonesten <v@tmplt.dev>
2021-11-21 01:22:27 +0100

    tpiu: remove get_ prefix, as per Rust API guidelines

--- src/peripheral/tpiu.rs
+++ src/peripheral/tpiu.rs
@@ -108,7 +108,7 @@ impl TPIU {
     /// Reads the supported trace output modes and the minimum size of
     /// the TPIU FIFO queue for trace data.
     #[inline]
-    pub fn get_swo_supports() -> SWOSupports {
+    pub fn swo_supports() -> SWOSupports {
         let _type = unsafe { (*Self::ptr())._type.read() };
         SWOSupports {
             nrz_encoding: _type.nrzvalid(),

c1d434a7e7ee1a04ce06c3e3bcdb9b65e048b8ba
Viktor Sonesten <v@tmplt.dev>
2021-11-27 13:34:28 +0100

    bump MSRV
    Required to support the #[non_exhaustive] attribute.

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@
 //!
 //! # Minimum Supported Rust Version (MSRV)
 //!
-//! This crate is guaranteed to compile on stable Rust 1.38 and up. It *might*
+//! This crate is guaranteed to compile on stable Rust 1.40 and up. It *might*
 //! compile with older versions but that may change in any new patch release.
 
 #![cfg_attr(feature = "inline-asm", feature(asm))]

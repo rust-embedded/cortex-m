cb6eb03e981b896b892c40483c051b8c52ec95f6
James Munns <james@onevariable.com>
2024-03-14 17:12:43 +0100

    Update critical_section.rs

--- cortex-m/src/critical_section.rs
+++ cortex-m/src/critical_section.rs
@@ -9,6 +9,7 @@ set_impl!(SingleCoreCriticalSection);
 unsafe impl Impl for SingleCoreCriticalSection {
     unsafe fn acquire() -> RawRestoreState {
         let was_active = primask::read().is_active();
+        // NOTE: Fence guarantees are provided by interrupt::disable(), which performs a `compiler_fence(SeqCst)`.
         interrupt::disable();
         was_active
     }
@@ -16,6 +17,8 @@ unsafe impl Impl for SingleCoreCriticalSection {
     unsafe fn release(was_active: RawRestoreState) {
         // Only re-enable interrupts if they were enabled before the critical section.
         if was_active {
+            // NOTE: Fence guarantees are provided by interrupt::disable(), which performs a
+            // `compiler_fence(SeqCst)`.
             interrupt::enable()
         }
     }

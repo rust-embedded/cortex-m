6e1edffa36286a1c5600141bbc426c3f2dbeb7db
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2024-07-02 17:30:31 -0300

    cortex-m: macros: Fix typos in documentation
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

--- cortex-m/src/macros.rs
+++ cortex-m/src/macros.rs
@@ -36,8 +36,8 @@ macro_rules! iprintln {
 /// you can enable the `critical-section-single-core` feature for this crate. For other systems, you
 /// have to provide one from elsewhere, typically your chip's HAL crate.
 ///
-/// For debuggability, you can set an explicit name for a singleton.  This name only shows up the
-/// the debugger and is not referenceable from other code.  See example below.
+/// For debuggability, you can set an explicit name for a singleton. This name only shows up the
+/// debugger and is not referenceable from other code. See example below.
 ///
 /// # Example
 ///

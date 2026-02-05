41b3882f1a756a5fc565e6f7d2ada10a82dc9208
Andres <agrc14@gmail.com>
2026-02-05 13:21:11 +0100

    fix: Unsafe at function level, refmt

--- cortex-m/src/coprocessor.rs
+++ cortex-m/src/coprocessor.rs
@@ -11,21 +11,19 @@
 ///  - CRM: Coprocessor register M.
 ///  - OP2: Second optional operation for the coprocessor.
 #[inline(always)]
-pub fn mcr<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const OP2: u32>(
+pub unsafe fn mcr<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const OP2: u32>(
     value: u32,
 ) {
-    unsafe {
-        core::arch::asm!(
-            "MCR p{cp}, #{op1}, {0}, c{crn}, c{crm}, #{op2}",
-            in(reg) value,
-            cp  = const CP,
-            op1 = const OP1,
-            crn = const CRN,
-            crm = const CRM,
-            op2 = const OP2,
-            options(nostack, nomem)
-        )
-    }
+    core::arch::asm!(
+        "MCR p{cp}, #{op1}, {0}, c{crn}, c{crm}, #{op2}",
+        in(reg) value,
+        cp  = const CP,
+        op1 = const OP1,
+        crn = const CRN,
+        crm = const CRM,
+        op2 = const OP2,
+        options(nostack, nomem)
+    );
 }
 
 /// This instruction moves one Coprocessor Register to a Register.
@@ -39,21 +37,20 @@ pub fn mcr<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const 
 ///  - CRM: Coprocessor register M.
 ///  - OP2: Second optional operation for the coprocessor.
 #[inline(always)]
-pub fn mrc<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const OP2: u32>() -> u32 {
+pub unsafe fn mrc<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const OP2: u32>(
+) -> u32 {
     let a: u32;
 
-    unsafe {
-        core::arch::asm!(
-            "MRC p{cp}, #{op1}, {0}, c{crn}, c{crm}, #{op2}",
-            out(reg) a,
-            cp  = const CP,
-            op1 = const OP1,
-            crn = const CRN,
-            crm = const CRM,
-            op2 = const OP2,
-            options(nostack, nomem)
-        )
-    }
+    core::arch::asm!(
+        "MRC p{cp}, #{op1}, {0}, c{crn}, c{crm}, #{op2}",
+        out(reg) a,
+        cp  = const CP,
+        op1 = const OP1,
+        crn = const CRN,
+        crm = const CRM,
+        op2 = const OP2,
+        options(nostack, nomem)
+    );
 
     a
 }
@@ -67,18 +64,16 @@ pub fn mrc<const CP: u32, const OP1: u32, const CRN: u32, const CRM: u32, const 
 ///  - OP1: First optional operation for the coprocessor.
 ///  - CRM: Coprocessor register M.
 #[inline(always)]
-pub fn mcrr<const CP: u32, const OP1: u32, const CRM: u32>(a: u32, b: u32) {
-    unsafe {
-        core::arch::asm!(
-            "MCRR p{cp}, #{op1}, {0}, {1}, c{crm}",
-            in(reg) a,
-            in(reg) b,
-            cp  = const CP,
-            op1 = const OP1,
-            crm = const CRM,
-            options(nostack, nomem)
-        )
-    }
+pub unsafe fn mcrr<const CP: u32, const OP1: u32, const CRM: u32>(a: u32, b: u32) {
+    core::arch::asm!(
+        "MCRR p{cp}, #{op1}, {0}, {1}, c{crm}",
+        in(reg) a,
+        in(reg) b,
+        cp  = const CP,
+        op1 = const OP1,
+        crm = const CRM,
+        options(nostack, nomem)
+    );
 }
 
 /// This instruction moves two Coprocessor Registers to Registers.
@@ -90,22 +85,20 @@ pub fn mcrr<const CP: u32, const OP1: u32, const CRM: u32>(a: u32, b: u32) {
 ///  - OP1: First optional operation for the coprocessor.
 ///  - CRM: Coprocessor register M.
 #[inline(always)]
-pub fn mrrc<const CP: u32, const OPC: u32, const CRM: u32>() -> (u32, u32) {
+pub unsafe fn mrrc<const CP: u32, const OPC: u32, const CRM: u32>() -> (u32, u32) {
     // Preallocate the values.
     let a: u32;
     let b: u32;
 
-    unsafe {
-        core::arch::asm!(
-            "MRRC p{cp}, #{opc}, {0}, {1}, c{crm}",
-            out(reg) a,
-            out(reg) b,
-            cp  = const CP,
-            opc = const OPC,
-            crm = const CRM,
-            options(nostack, nomem)
-        )
-    }
+    core::arch::asm!(
+        "MRRC p{cp}, #{opc}, {0}, {1}, c{crm}",
+        out(reg) a,
+        out(reg) b,
+        cp  = const CP,
+        opc = const OPC,
+        crm = const CRM,
+        options(nostack, nomem)
+    );
 
     (a, b)
 }

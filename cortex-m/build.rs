1ef49e9ed7a087b392f9655bac07abc3c48d89bc
Wren Turkal <wt@penguintechs.org>
2026-03-12 16:25:17 -0700

    Document compatibility of the `cm7-r0p1` feature.
    The `cm7-r0p1` feature only works with the plateform feature `armv7em`.
    I added a compile error to help someone figure that out more redily if
    they accidentally combine those features.

--- cortex-m/build.rs
+++ cortex-m/build.rs
@@ -47,7 +47,7 @@ fn main() {
     } else if target.starts_with("thumbv7em-") {
         println!("cargo:rustc-cfg=cortex_m");
         println!("cargo:rustc-cfg=armv7m");
-        println!("cargo:rustc-cfg=armv7em"); // (not currently used)
+        println!("cargo:rustc-cfg=armv7em");
     } else if target.starts_with("thumbv8m.base") {
         println!("cargo:rustc-cfg=cortex_m");
         println!("cargo:rustc-cfg=armv8m");
